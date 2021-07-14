a16611c3a322b2ca421ae53e7ef7b1d9fb3137a4
cranium44 <adabeechaim@gmail.com>
2021-07-14 14:33:30 +0100

    Initial commit

---
+++ app/src/main/java/live/adabe/fragmentmanager/fragments/HomeFragment.kt
@@ -0,0 +1,23 @@
+package live.adabe.fragmentmanager.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import live.adabe.fragmentmanager.R
+import live.adabe.fragmentmanager.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
