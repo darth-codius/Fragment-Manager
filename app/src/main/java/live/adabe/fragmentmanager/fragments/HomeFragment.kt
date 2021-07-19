b3b1c1a10891e62df7cadc59f3faba5b19cc4aeb
cranium44 <adabeechaim@gmail.com>
2021-07-19 12:41:31 +0100

    Implement fragment manager

--- app/src/main/java/live/adabe/fragmentmanager/fragments/HomeFragment.kt
+++ app/src/main/java/live/adabe/fragmentmanager/fragments/HomeFragment.kt
@@ -5,19 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import live.adabe.fragmentmanager.R
 import live.adabe.fragmentmanager.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var manager: FragmentManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        manager = requireActivity().supportFragmentManager
+        binding.apply {
+            nextButton.setOnClickListener {
+                openNextFragmentKt()
+            }
+        }
         return binding.root
+    }
+
+    private fun openNextFragmentKt() {
+        manager.commit {
+            replace<NewFragment>(R.id.fragmentContainerView)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
+    }
+
+    private fun openNextFragmentJv() {
+        manager.beginTransaction()
+            .replace(R.id.fragmentContainerView, NewFragment::class.java, null)
+            .setReorderingAllowed(true)
+            .addToBackStack(null)
+            .commit()
     }
 
 }
